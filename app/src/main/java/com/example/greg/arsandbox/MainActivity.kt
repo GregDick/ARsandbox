26e61602b74222e3efef9fc4420946b5d974ec4b
Greg Dick <gdick@mercuryintermedia.com>
2020-05-22 13:59:45 -0500

    setup and permission handling. Added sceneviewer

--- app/src/main/java/com/example/greg/arsandbox/MainActivity.kt
+++ app/src/main/java/com/example/greg/arsandbox/MainActivity.kt
@@ -1,12 +1,79 @@
 package com.example.greg.arsandbox
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.greg.arsandbox.util.CameraPermissionHelper
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
+
 
 class MainActivity : AppCompatActivity() {
+
+    // Set to true ensures requestInstall() triggers installation if necessary.
+    private var userRequestedInstall = true
+    private var session : Session? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this)
+        }
+
+        // Make sure Google Play Services for AR is installed and up to date.
+        try {
+            if (session == null) {
+                when (ArCoreApk.getInstance().requestInstall(this, userRequestedInstall)) {
+                    ArCoreApk.InstallStatus.INSTALLED -> {
+                        session = Session(this)
+                    }
+                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                        userRequestedInstall = false
+                        return
+                    }
+                }
+            }
+        } catch (e: UnavailableUserDeclinedInstallationException) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+
+        val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
+        sceneViewerIntent.data =
+            Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf")
+        sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox")
+        startActivity(sceneViewerIntent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(
+                this,
+                "Camera permission is needed to run this application",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+            // Permission denied with checking "Do not ask again".
+            CameraPermissionHelper.launchPermissionSettings(this)
+        }
+        finish()
     }
 }
